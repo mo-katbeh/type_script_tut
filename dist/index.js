595378c86921f138ca9fa961d87341bf41351d7b
mo-katbeh <hamode.katbeh@gmail.com>
2025-06-22 19:23:17 +0300

    update of section 4

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,39 @@
 "use strict";
-let age = 20;
-if (age < 50)
-    age += 10;
-console.log(age);
+let sales = 20099999;
+let level;
+let queue = [1, 3, 4, 5, 6, 7, 8, 8];
+queue.forEach(element => {
+    console.log(element.toString);
+});
+let user = [1, 'mm'];
+function render(resposne) {
+    console.log(resposne);
+}
+var Size;
+(function (Size) {
+    Size[Size["Small"] = 3] = "Small";
+    Size[Size["Medium"] = 4] = "Medium";
+    Size[Size["Large"] = 5] = "Large";
+})(Size || (Size = {}));
+;
+let mySize = Size.Large;
+console.log(mySize);
+function calculateTax(income, taxYear) {
+    if ((taxYear || 2022) < 50000)
+        return income * 1.2;
+    return income;
+}
+calculateTax(22);
+let employee = {
+    id: 1,
+    name: "",
+    retire: (date) => {
+        console.log(date);
+    }
+};
+function kgToLbs(weight) {
+    if (typeof weight === 'number')
+        return weight * 2.2;
+    return parseInt(weight) * 2.2;
+}
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,60 @@
-let age: number =20;
-if(age<50)
-    age+=10;
-console.log(age)
+let sales = 200_999_99
+let level
+let queue = [1,3,4,5,6,7,8,8];
+
+queue.forEach(element => {
+    console.log(element.toString);
+});
+// console.log(level);
+// tuple
+let user :[number, string] = [1, 'mm']
+function render(resposne :any){
+    console.log(resposne);  
+}
+//enum
+enum Size{Small =3, Medium, Large};
+let mySize: Size = Size.Large;
+console.log(mySize);
+function calculateTax(income: number, taxYear?:number): number{
+    if((taxYear||2022) < 50_000)
+        return income * 1.2;
+    return income;
+}
+calculateTax(22);
+
+type Employee = {
+    readonly id:number,
+    name:string
+    retire: (date: Date)=> void
+}
+let employee : Employee = {
+    id:1,
+    name:"",
+    retire:(date:Date)=>{
+           console.log(date);
+       }
+    };
+
+    function kgToLbs(weight:number | string):number{
+        // Narrowing
+        if(typeof weight === 'number')
+            return weight * 2.2;
+        return parseInt(weight)*2.2;
+    }
+
+type Draggable = {
+    drag: ()=> void
+};
+type resizeable = {
+    resize: ()=> void
+}
+type UIWidget = Draggable & resizeable
+
+let ui: UIWidget = {
+    resize:()=> {},
+    drag:()=> {}
+}
+type Hight = 150|200;
+let hight :Hight =200
+
+type Metric = 'cm' | 'm'
